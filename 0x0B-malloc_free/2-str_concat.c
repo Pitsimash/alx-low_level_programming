9804054b197c00ee3d6453238813eb9cad655604
pitsimash <pitsimashitisho@gmail.com>
2023-07-10 09:45:22 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,14 +16,13 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
-	
 	if (s2 == NULL)
 		s2 = "";
 
 	while (s1[i])
 		i++;
 
-	while(s2[j])
+	while (s2[j])
 		j++;
 
 	l = i + j;
@@ -38,7 +37,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		if (k <= i)
 			s[k] = s1[k];
-		
+
 		if (k >= i)
 		{
 			s[k] = s2[j];
